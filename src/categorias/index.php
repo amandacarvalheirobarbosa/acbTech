9af750d0ee145c8bbf52f64b70ecff8ff3e19763
Amanda Carvalheiro Barbosa <acarvalheirobarbosa@gmail.com>
2024-04-09 21:36:19 -0300

    style: layout

--- src/categorias/index.php
+++ src/categorias/index.php
@@ -3,11 +3,10 @@
 include_once("../../db/connection.php");
 
 try {
-  $sql = "SELECT * FROM tab_categoria where delete_date IS NULL";
+  $sql = "SELECT * FROM tab_categoria where deleted IS NULL";
   $stmt = $conn->prepare($sql);
   $stmt->execute();
   $result = $stmt->get_result();
-
 } catch (Exception $e) {
   $erro = $e->getMessage();
 }
@@ -43,99 +42,6 @@ try {
       showMeridian: 1
     });
   });
-
-  function SubmeterForm() {
-    $("#FormLog").submit();
-  }
-
-  function VoltarAdicionar() {
-    $("#ModalAdicionar").modal("hide");
-  }
-
-  function VoltarExcluir() {
-    $("#ModalExcluir").modal("hide");
-  }
-
-  function Adicionar() {
-    $("#ModalAdicionar").modal("show");
-
-    $("#eId").val(0);
-    $("#eNome").val("");
-  }
-
-  function GravarCategoria() {
-    var dataU = {
-      Id: $("#eId").val(),
-      Nome: $('#eNome').val()
-    };
-
-    if (!dataU.Nome) {
-      $("#eError").text("É necessário preencher o campo nome");
-      return;
-    }
-
-    $.ajax({
-      url: '../../api/categoria/insert.php',
-      method: "POST",
-      data: dataU,
-      success: function (response) {
-        console.log('Resposta do servidor:', response);
-      },
-      error: function (xhr, status, error) {
-        console.error('Erro na requisição:', error);
-      }
-    });
-    return;
-  }
-
-  function Editar(id) {
-    $("#ModalAdicionar").modal("show");
-
-    $.ajax({
-      url: "../../api/categoria/query.php",
-      type: "POST",
-      data: {
-        IdCategoria: id,
-      },
-      success: function (response) {
-        var ob = JSON.parse(response);
-        $("#eId").val(ob.id_categoria);
-        $("#eNome").val(ob.nome);
-      },
-      error: function (xhr, ajaxOptions, thrownError) {
-        alert(thrownError);
-      },
-    });
-    return;
-  }
-
-  function Excluir(id) {
-    $("#ModalExcluir").modal("show");
-    $("#IdExclude").val(id);
-  }
-
-  function ExcluirCategoria() {
-    var dataU = {};
-    dataU.Id = $("#IdExclude").val();
-
-    $.ajax({
-      url: "../../api/categoria/delete.php",
-      type: "POST",
-      data: dataU,
-      success: function (response) {
-        if (response.indexOf("sucesso") != -1) {
-          $("#ModalExcluir").modal("hide");
-          $("#FormLog").submit();
-        } else {
-          $("#eError").text(response);
-        }
-      },
-      error: function (xhr, ajaxOptions, thrownError) {
-        $("#eError").text(thrownError);
-      },
-    });
-    return;
-  }
 </script> -->
 
 <body>
